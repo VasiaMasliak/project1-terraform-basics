d9cff6104976f57992416bcfb89b53bfa046a177
VasiaMasliak <vasia.masliak@gmail.com>
2025-09-22 21:24:25 +0200

    IaaC: Storage Account + VM on Azure

---
+++ azure/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "public_key_path" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "storage_name" {
+  type = string
+}
